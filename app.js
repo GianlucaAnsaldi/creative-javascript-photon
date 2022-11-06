7750b4bc3b58c5dabd6903c336b1ef04feff8d93
Gianluca <gianluca.ansaldi.1989@gmail.com>
2022-11-06 00:05:23 +0000

    finishing up functionalities

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
 const form = document.querySelector('.search-form');
+const more = document.querySelector('.more');
+
 let searchValue;
+let page = 1;
+let fetchLink;
+let currentSearch;
 
 async function curatedPhotos() {
-  const data = await fetchApi(
-    'https://api.pexels.com/v1/curated?per_page=15&page=1'
-  );
+  fetchLink = 'https://api.pexels.com/v1/curated?per_page=15&page=1';
+  const data = await fetchApi(fetchLink);
   generatePictures(data);
 }
 
 async function searchPhotos(search) {
   clear();
-  const data = await fetchApi(
-    `https://api.pexels.com/v1/search?query=${search}&per_page=15&page=1`
-  );
+  fetchLink = `https://api.pexels.com/v1/search?query=${search}&per_page=15&page=1`;
+  const data = await fetchApi(fetchLink);
   generatePictures(data);
 }
 
@@ -30,6 +33,17 @@ async function fetchApi(url) {
   });
   const data = await dataFetch.json();
   return data;
+}
+
+async function loadMore() {
+  page++;
+  if (currentSearch) {
+    fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page}`;
+  } else {
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+  }
+  const data = await fetchApi(fetchLink);
+  generatePictures(data);
 }
 
 const generatePictures = (data) => {
@@ -55,7 +69,9 @@ const clear = () => {
 searchInput.addEventListener('input', updateInput);
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  currentSearch = searchValue;
   searchPhotos(searchValue);
 });
+more.addEventListener('click', loadMore);
 
 curatedPhotos();
